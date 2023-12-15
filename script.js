062ffae41337d5502f33b2d8344890680cbc506d
josephkeelagher <KeelagherJ@gmail.com>
2023-12-15 23:06:44 +1100

    Finished website course

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+function openLinkedIn() {
+    window.open('https://www.linkedin.com/in/joseph-keelagher-b67170292/', '_blank');
+}
+function openGitHub() {
+    window.open('https://github.com/josephkeelagher', '_blank');
+}
+function openProjectOne() {
+    windown.open('https://github.com/josephkeelagher', '_blank');
+}
