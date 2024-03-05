ce6720bd23e51c778cbe9bece04399458bc8e84b
josephkeelagher <KeelagherJ@gmail.com>
2024-03-05 17:27:43 +1100

    Final touches, projects added in

--- script.js
+++ script.js
@@ -8,8 +8,19 @@ function toggleMenu() {
 function openLinkedIn() {
     window.open('https://www.linkedin.com/in/joseph-keelagher-b67170292/', '_blank');
 }
-function openGitHub() {
-    window.open('https://github.com/josephkeelagher', '_blank');
+function openGitHub(input) {
+    if (input) {
+        switch (input) {
+            case 'P1':
+                window.open('https://github.com/josephkeelagher/AFL-brownlow-predictor', '_blank');
+            case 'P2':
+                window.open('https://github.com/josephkeelagher/ShadowDance-game', '_blank');
+            case 'P3':
+                window.open('https://github.com/josephkeelagher/mock-database-concert', '_blank');
+
+        }
+    }
+    else {window.open('https://github.com/josephkeelagher', '_blank');}
 }
 function openProjectOne() {
     windown.open('https://github.com/josephkeelagher', '_blank');
