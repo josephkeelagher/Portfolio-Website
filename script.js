9a117ccead5190d466e07bc6a709ac0f1db69139
josephkeelagher <KeelagherJ@gmail.com>
2024-07-23 16:18:18 +1000

    Expanded Projects section, flex fixes across whole site

--- script.js
+++ script.js
@@ -17,7 +17,12 @@ function openGitHub(input) {
                 window.open('https://github.com/josephkeelagher/ShadowDance-game', '_blank');
             case 'P3':
                 window.open('https://github.com/josephkeelagher/mock-database-concert', '_blank');
-
+            case 'P4':
+                window.open('https://github.com/josephkeelagher/IDMB-Predictor-Machine-Learning', '_blank');
+            case 'P5':
+                window.open('https://github.com/josephkeelagher/AI-Tetress-Agent', '_blank');
+            case 'P6':
+                window.open('https://github.com/josephkeelagher/Portfolio-Website', '_blank');
         }
     }
     else {window.open('https://github.com/josephkeelagher', '_blank');}
